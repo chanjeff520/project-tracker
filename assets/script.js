05c1f548bbb08e4e20908937ebb15fcad7f1c63f
chanjeff520 <chanjeff520@gmail.com>
2022-10-05 15:10:55 -0400

    add working time

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
 
-var time = moment().format("MMM Do, yyyy");
 
-$("#currentTime").append(time);
+function keepTrackOfTime(){
+    var time = moment().format("MMM Do, yyyy") +" "+ moment().format("hh:mm:ss");
+    //$("#currentTime").append(time);
+    $("#currentTime").text(time);
+}
+
+
+
+
+keepTrackOfTime();
+var timerInterval =  setInterval(function() {
+    keepTrackOfTime()
+},1000);
